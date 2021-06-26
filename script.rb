da3b6312f87f34efc3941631f002d2ef5313d560
Liam Hayden <l.hayden67@outlook.com>
2021-06-26 19:26:03 +0100

    Updated code

--- script.rb
+++ script.rb
@@ -3,7 +3,15 @@
 def caesar_cipher(string, shift_factor)
     arr = string.split('')
     result = []
-    arr.each { |letter| result.push((letter.ord + shift_factor).chr)}
+    arr.each { |letter| 
+        if letter.ord.between?(65, 90)
+            result.push((((letter.ord - 65) + shift_factor) % 26 + 65).chr) 
+        elsif letter.ord.between?(97, 122)
+            result.push((((letter.ord - 97) + shift_factor) % 26 + 97).chr)
+        else 
+            result.push(letter)
+        end 
+    }
     result.join("")
 end 
 
